8d137bb80cd4ea97a7ba9a28c3cba73059e2679a
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-07-21 19:14:42 +0530

    added codes

--- practice.cpp
+++ practice.cpp
@@ -1,22 +1,10 @@
 #include<iostream>
 using namespace std;
-class MinHeap{
-    public:
-        int arr[50];
-        int idx;
-        MinHeap(){
-            idx = 1;
-        }
-        int top(){
-            return arr[1];
-        }
-        void push(int x){
-            arr[idx] = x;
-            int i = idx;
-            idx++;
-        }
-};
 int main(){
-
-    return 0;
+    int a =10;
+    int b = 20;
+    a = a*b;
+    b= a/b;
+    a= a/b;
+    cout<<a<<" "<<b;
 }
