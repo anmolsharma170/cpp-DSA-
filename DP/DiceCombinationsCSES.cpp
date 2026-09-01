7d8ed915386a78a6c5f98886ad449247654e0b53
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-09-01 17:58:04 +0530

    solved dice combination CSES

--- DP/DiceCombinationsCSES.cpp
+++ DP/DiceCombinationsCSES.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 #include<vector>
-#define ll long long
-ll func(int n){
-    if(n==0) return -1;
-    int sum = 0;
-    for(int i = 0;i<=6;i++){
-        
+#define ll long long int
+#define mod 1000000007
+using namespace std;
+vector<ll> dp(1000006,-1);
+int f(int n){
+    if(n==0) return 1;
+    if(dp[n]!=-1) return dp[n];
+    ll sum = 0;
+    for(int i=1;i<=6;i++){
+        if(n-i<0) break;
+        sum = (sum % mod + f(n-i)%mod)%mod;
     }
+    return dp[n]=sum%mod;
 }
 int main(){
-
+    int n;
+    cin>>n;
+    cout<<f(n);
 }
